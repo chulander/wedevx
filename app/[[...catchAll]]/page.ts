fda461c4cb55d08fa14586602edd36b359402509
Bryan Chu <bryan.chu@allhere.com>
2025-03-05 04:38:56 -0500

    added catch all route

---
+++ app/[[...catchAll]]/page.ts
@@ -0,0 +1,20 @@
+// import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+interface CatchAllParams {
+  params: Promise<{ catchAll?: string[] }>;
+}
+
+export default async function CatchAllPage({ params }: CatchAllParams) {
+  const { catchAll } = await params;
+
+  // Attempted path reconstruction
+  const attemptedPath = catchAll ? catchAll.join("/") : null;
+  if (attemptedPath) {
+    console.error(`User tried to access: /${attemptedPath}`);
+  }
+
+  redirect("/visa-applications");
+
+  return null; // This will never be reached due to `redirect`, but is required for TypeScript.
+}
